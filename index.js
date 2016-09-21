32bcc9c5b271486c2c42e5ec8ce0b71fd42a72da
simonglew <sglew1@gmail.com>
2016-09-21 21:53:09 +1200

    Advanced Calculator logic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var buttonPressed = false;
 var operation = "+";
 var MAX_LENGTH = 10;
 
-function AddDigit(digit){
+function addDigit(digit){
 	if(!buttonPressed){
 		displayedValue = digit;
 		buttonPressed = true;
@@ -14,14 +14,33 @@ function AddDigit(digit){
 	document.getElementById('displayNumbers').innerHTML = displayedValue;
 }
 
-function Clear(){
+function changeSign(){
+	if(displayedValue.indexOf('-') == 0){
+		displayedValue = displayedValue.substring(1);
+	}else{
+		displayedValue = '-' + displayedValue
+	}
+	document.getElementById('displayNumbers').innerHTML = displayedValue;
+}
+
+function percentage(){
+	displayedValue = displayedValue / 100;
+	document.getElementById('displayNumbers').innerHTML = displayedValue;
+}
+
+function decimalPoint(){
+	displayedValue = displayedValue +'.';
+	document.getElementById('displayNumbers').innerHTML = displayedValue;
+}
+
+function clear(){
 	displayedValue = 0;
 	memoryValue = 0;
 	buttonPressed = 0;
 	document.getElementById('displayNumbers').innerHTML = displayedValue;
 }
 
-function Operate(operator){
+function operate(operator){
 	operation = operator;
 	buttonPressed = false;
 	firstValue = displayedValue;
@@ -29,7 +48,7 @@ function Operate(operator){
 	document.getElementById('displayNumbers').innerHTML = displayedValue;
 }
 
-function Calculate(){
+function calculate(){
 	var secondValue = displayedValue;
 	if(operation == '+'){
 		displayedValue = eval(firstValue) + eval(secondValue);
